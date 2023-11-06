c863f179bf5ffc22246963a2379773d24073c908
Virul Nirmala <89099391+iamvirul@users.noreply.github.com>
2023-11-06 22:34:46 +0530

    YouTube video download location path name set

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ def startDownload():
         ytlink = str(link.get())
         youtubeobject = YouTube(ytlink)
         video = youtubeobject.streams.get_highest_resolution()
-        video.download()
+        video.download('~/download')
     except Exception as e:
         finishLabel.configure(text=e, text_color="red")
-        print(e)
+    finishLabel.configure(text="Downloaded", text_color="Green")
 
 
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()
-app.geometry("720x480")
+app.geometry("820x480")
 app.title("Youtube Downloader")
 
 title = customtkinter.CTkLabel(app, text="Insert Your YouTube Link")
 title.pack(padx=10, pady=10)
 
 url_var = tkinter.StringVar()
-link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link = customtkinter.CTkEntry(app, width=400, height=40, textvariable=url_var)
 link.pack()
 
 finishLabel = customtkinter.CTkLabel(app, text="")
